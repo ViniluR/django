e4b48de34a3069bf6759e0395962dddad7d9cb2b
Vinícius Henrique de Souza Pinheiro <20221011110006@ifrn.local>
2024-03-25 07:51:30 -0300

    ai papai agr vai

--- lojaAdmin/settings.py
+++ lojaAdmin/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--@q81ap$%h#3fqqot15-2z@s4%g%d9je=$o5#a6l&^a+ilbq1y'
+SECRET_KEY = 'django-insecure-w-s1!2wmfji^7b#m@q^6nh%s&dnotoro$-pktv=2fo3(8t$yfv'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -35,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'loja',
 ]
 
 MIDDLEWARE = [
@@ -102,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
