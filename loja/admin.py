ae3061135d99aef43aea0fc4b9f2ccfbeb5a6b2c
Vinícius Henrique de Souza Pinheiro <20221011110006@ifrn.local>
2024-07-12 10:11:50 -0300

    pesquisar

--- loja/admin.py
+++ loja/admin.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 from .models import *
 class FabricanteAdmin(admin.ModelAdmin):
 # Cria um filtro de hierarquia com datas
+    search_fields = ('Fabricante',)
     date_hierarchy = 'criado_em'
+    
 class ProdutoAdmin(admin.ModelAdmin):
+    search_fields = ('Produto',)
     date_hierarchy = 'criado_em'
     list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
-    empty_value_display = 'Vazio'
+    empty_value_display = 'Sem promoção'
+class CategoriaAdmin(admin.ModelAdmin):
+    search_fields = ('Categoria',)
+    date_hierarchy = 'criado_em'
+    
 admin.site.register(Fabricante,FabricanteAdmin)
-admin.site.register(Categoria)
+admin.site.register(Categoria,CategoriaAdmin)
 admin.site.register(Produto,ProdutoAdmin)
