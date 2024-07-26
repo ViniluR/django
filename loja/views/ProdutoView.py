cd8125d84aa572e844fd86313cfb5f320ca40f2c
Vinícius Henrique de Souza Pinheiro <20221011110006@ifrn.local>
2024-07-26 10:27:11 -0300

    cap 5 e 6

---
+++ loja/views/ProdutoView.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from loja.models import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
+def list_produto_view(request, id=None):
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+    dias = request.GET.get("dias")
+    produtos = Produto.objects.filter(Produto=produto)
+    produtos = Produto.objects.all()
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    if categoria is not None:
+        produtos = produtos.filter(categoria__Categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    print(produtos)
+    if id is None:
+        return HttpResponse('<h1>Nenhum id foi informado</h1>')
+    return HttpResponse('<h1>Produto de id %s!</h1>' % id) 
