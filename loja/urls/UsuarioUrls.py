5b11dc41291e9205e3965dc561e417b775296065
Vinícius Henrique de Souza Pinheiro <20221011110006@ifrn.local>
2024-10-14 08:09:27 -0300

    cap 9 pag 12

--- loja/urls/UsuarioUrls.py
+++ loja/urls/UsuarioUrls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from loja.views.UsuarioView import list_usuario_view
+from loja.views.UsuarioView import *
 urlpatterns = [
     path("", list_usuario_view, name='usuario'),
+    path("edit", edit_usuario_view, name='edit_usuario'),
 ]
